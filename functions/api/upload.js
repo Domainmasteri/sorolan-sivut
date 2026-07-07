be4b3aab925b8f0c91c7c19a3ce189730071b92d
Domainmasteri <markus.sorola.2@proton.me>
2026-07-07 17:22:50 +0300

    Increase max file size limit to 5120 MB

--- functions/api/upload.js
+++ functions/api/upload.js
@@ -3,7 +3,7 @@ export async function onRequestPost(context) {
         // ==========================================
         // SÄÄDÄ TIEDOSTORAJOTUS TÄSTÄ (Megatavuina)
         // ==========================================
-        const MAX_SIZE_MB = 50; 
+        const MAX_SIZE_MB = 5120; 
         const MAX_SIZE_BYTES = MAX_SIZE_MB * 1024 * 1024;
 
         const request = context.request;
